d67f81ff68a3cd4ee5f0fdcaf93e34348a22d337
abodzsar29 <abodzsar29@gmail.com>
2024-07-12 23:22:49 +0100

    Solved LeetCode linked list problem & Added comments to LinkedList practice file

--- ADS/LinkedLists/LinkedLists.cpp
+++ ADS/LinkedLists/LinkedLists.cpp
@@ -7,6 +7,19 @@ public:
 	Node* next;
 };
 
+// Passing in pointers by pointer * vs reference *&:
+/*
+By pointer *:
+The function receives a copy of the pointer. Both the original pointer and the copied pointer point to the same memory address,
+meaning they refer to the same node. Modifications to the pointer itself within the function do not affect the original pointer
+passed from the caller. The object that the pointer points to can be modified. The original pointer cannot be changed to point to a different node.
+
+By reference *&:
+Passing the actual pointer itself. This allows the function to modify the original pointer, making it possible to change what node the pointer points to.
+The original pointer is modified, not just the object it points to. Useful when the function needs to update the pointer to point to a different 
+node, such as updating the head of a linked list.
+*/
+
 // Outputting the data attribute of the Node
 void iterate(Node* n) {
 	while (n != nullptr) {
@@ -45,7 +58,7 @@ static void pushToTail(Node*& head, int val) {
 }
 
 // Inserting after a specific Node
-static void insertAfter(Node* prev_node, int new_data) {
+static void insertAfter(Node*& prev_node, int new_data) {
 	if (prev_node == nullptr) {
 		std::cout << "The given previous node cannot be NULL" << std::endl;
 		return;
@@ -82,7 +95,9 @@ int main() {
 	cout << endl;
 	pushToTail(head, 99);
 	iterate(head);
-	
+	cout << endl;
+	insertAfter(second, 69);
+	iterate(head);
 
 	// Cleaning up memory
 	while (head != nullptr) {
