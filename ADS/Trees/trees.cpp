932fd38d0871bb972bcb6cf122fcb2fe25cd94b8
Andrew Bodzsar <abodzsar29@gmail.com>
2025-01-11 14:43:05 +0100

    Implemented Breadth-First Traverse

--- ADS/Trees/trees.cpp
+++ ADS/Trees/trees.cpp
@@ -1,17 +1,66 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 struct TreeNode {
-    public:
-        int val;
-        TreeNode* left;
-        TreeNode* right;
-        TreeNode(): val(0), left(nullptr), right(nullptr) {};
-        TreeNode(int x): val(x), left(nullptr), right(nullptr) {};
-        TreeNode(int x, TreeNode* left, TreeNode* right): val(x), left(left), right(right) {};
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {};
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {};
+    TreeNode(int x, TreeNode* left, TreeNode* right): val(x), left(left), right(right) {};
 };
- 
+
+class BinaryTree {
+private:
+    TreeNode* root;
+
+    void deleteNodes(TreeNode* node) {
+        if (node == nullptr) {
+            return;
+        }
+        deleteNodes(node->left);
+        deleteNodes(node->right);
+        delete node;
+    }
+
+public:
+    BinaryTree() : root(nullptr) {};
+    ~BinaryTree() {
+        deleteNodes(root);
+    };
+
+    TreeNode* getRoot() { return root;};
+
+    std::vector<std::vector<int>> BFS() {
+        std::vector<std::vector<int>> result;
+        if (!root) return result;
+        std::queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()) {
+            std::vector<int> currentLevel = {};
+            int levelSize = q.size();
+            
+            for(int i = 0; i < levelSize; i++) {
+                TreeNode* node = q.front();
+                currentLevel.push_back(node->val);
+                q.pop();
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            result.push_back(currentLevel);
+        }
+        return result;
+    }
+
+};
+
 
 int main() {
+
 
 
   return 0;
